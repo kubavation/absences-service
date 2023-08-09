20637e1167945cd68f37ddff95493fa149118a5c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-08-09 16:09:55 +0200

    init

---
+++ src/main/kotlin/com/durys/jakub/absencesservice/AbsencesServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.durys.jakub.absencesservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AbsencesServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AbsencesServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/durys/jakub/absencesservice/AbsencesServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.absencesservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AbsencesServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
